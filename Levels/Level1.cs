fda280dd37155d6716e3b0437b5d9035d8c4820f
Jiri Javora <javora.jiri@gmail.com>
2023-10-23 22:47:12 -0500

    Add movement instructions to first level

--- Levels/Level1.cs
+++ Levels/Level1.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections.Generic;
 using JACE.Common;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace JACE.Levels;
 
 public class Level1 : GameLevel.GameLevel {
+    private static bool _instructionsSeen;
+    private static readonly string instructions = "Use WASD or the Arrow Keys to move around";
+    private static readonly string instructionsExit = "Press `Enter` to play";
+    private Texture2D instructionsBackground;
+    private SpriteFont instructionsFont;
+
     public override void Activate() {
         KillerShapeManager = new KillerShapeManager(ScreenManager.Game);
 
@@ -26,9 +33,14 @@ public class Level1 : GameLevel.GameLevel {
         WinArea = new WinArea(new Vector2(7f / 8f * LevelSizeX, 1f / 2f * LevelSizeY), "Reach me...");
 
         base.Activate();
+
+        instructionsFont = Content.Load<SpriteFont>("Minimal");
+        instructionsBackground = Content.Load<Texture2D>("Blank");
     }
 
     public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen) {
+        if (!_instructionsSeen) return;
+
         if (Player.BoundingRectangle.IsColliding(WinArea.BoundingRectangle)) {
             ScreenManager.RemoveScreen(this);
             ScreenManager.AddScreen(new Level2());
@@ -36,5 +48,46 @@ public class Level1 : GameLevel.GameLevel {
 
 
         base.Update(gameTime, otherScreenHasFocus, coveredByOtherScreen);
+    }
+
+    public override void HandleInput(GameTime gameTime, InputState input) {
+        if (input.Action && !_instructionsSeen) _instructionsSeen = true;
+
+
+        if (!_instructionsSeen) return;
+        base.HandleInput(gameTime, input);
+    }
+
+    public override void Draw(GameTime gameTime) {
+        base.Draw(gameTime);
+
+        if (!_instructionsSeen) {
+            var graphicsDevice = ScreenManager.GraphicsDevice;
+            var spriteBatch = ScreenManager.SpriteBatch;
+
+            var instructionsSize = instructionsFont.MeasureString(instructions);
+
+            var instructionsX = (ViewportHelper.ViewportWidth - instructionsSize.X) / 2;
+            var instructionsY = (ViewportHelper.ViewportHeight - instructionsSize.Y) / 2;
+
+            spriteBatch.Begin(blendState: BlendState.NonPremultiplied);
+
+            var backgroundColor = Color.DimGray;
+            backgroundColor.A = 240;
+            spriteBatch.Draw(instructionsBackground,
+                new Rectangle(0, 0, ViewportHelper.ViewportWidth, ViewportHelper.ViewportHeight), backgroundColor);
+
+            spriteBatch.DrawString(instructionsFont, instructions, new Vector2(instructionsX, instructionsY),
+                JaceColors.BackgroundColor);
+
+            var instructionsExitSize = instructionsFont.MeasureString(instructionsExit);
+            var instructionsExitX = (ViewportHelper.ViewportWidth - instructionsExitSize.X) / 2;
+
+            spriteBatch.DrawString(instructionsFont, instructionsExit,
+                new Vector2(instructionsExitX, instructionsY + instructionsExitSize.Y * 2),
+                JaceColors.TertiaryColor);
+
+            spriteBatch.End();
+        }
     }
 }
